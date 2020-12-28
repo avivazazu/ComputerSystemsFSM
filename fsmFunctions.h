f21befb1c23ae4fb32ec6b80a97424dde0e1ab37
Aviva Zazulia <aviva@zazulia.com>
2020-12-27 20:26:08 -0500

    Update fsmFunctions.h

--- fsmFunctions.h
+++ fsmFunctions.h
@@ -14,3 +14,13 @@ void interpretSecondFile(struct states fsmDef[], char* fileName, int returnArray
 void testInitialize();
 
 void testRegex();
+
+void testStrParser();
+
+void strParser(char line[], char **strTracker,char **currentStateInput, char **input, char **nextState);
+
+int fillOutStructs(struct states fsmDef[], int numberOfStates, int currentStateIndex,  int index, int nextStateInt);
+
+int getNextState(struct states fsmDef[], int stepCounter, int currentStateIndex, char line[]);
+
+
